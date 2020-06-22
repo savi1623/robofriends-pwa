538bba3ba0b1dea8db8f8c3436e13e889640de04
Savannah Williams <Savi@YeeHaw.local>
2020-06-22 14:14:46 -0600

    added tests for reducers

---
+++ .history/src/reducers.test_20200622140301.js
@@ -0,0 +1,28 @@
+import {
+  CHANGE_SEARCHFIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
+} from './constants';
+
+import * as reducers from './reducers.js';
+
+describe('searchRobots', () => {
+  const initialState = {
+    searchField: '',
+  };
+  it(' should return the initial state', () => {
+    expect(reducers.searchRobots(undefined, {})).toEqual({ searchField: '' });
+  });
+
+  it(' should handle the change search field', () => {
+    expect(
+      reducers.searchRobots(initialState, {
+        type: type.CHANGE_SEARCHFIELD,
+        payload: 'abc',
+      })
+    ).toEqual({
+      searchField: 'abc',
+    });
+  });
+});
---
+++ .history/src/reducers.test_20200622140558.js
@@ -0,0 +1,39 @@
+import {
+  CHANGE_SEARCHFIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
+} from './constants';
+
+import * as reducers from './reducers.js';
+
+describe('searchRobots', () => {
+  const initialState = {
+    searchField: '',
+  };
+  it(' should return the initial state', () => {
+    expect(reducers.searchRobots(undefined, {})).toEqual({ searchField: '' });
+  });
+
+  it(' should handle the change search field', () => {
+    expect(
+      reducers.searchRobots(initialState, {
+        type: CHANGE_SEARCHFIELD,
+        payload: 'abc',
+      })
+    ).toEqual({
+      searchField: 'abc',
+    });
+  });
+});
+
+describe('requestRobots', () => {
+  const initialStateRobots = {
+    robots: [],
+    isPending: true,
+  };
+
+  it(' should return the initial state', () => {
+    expect(reducers.requestRobots(undefined, {})).toEqual(initialStateRobots);
+  });
+});
